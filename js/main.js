cdd28e22ba75801118179936a9b64753c86030a9
Wiktoria <93823213+stravberry@users.noreply.github.com>
2022-11-16 23:00:05 +0100

    fixed navbar (closing after clicking)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 const menu_btn = document.querySelector('.hamburger')
 const mobile_menu = document.querySelector('.mobile-nav')
 
+const navButtonCheck = document.querySelector('.mobile-nav')
+
+
+
 menu_btn.addEventListener('click', function () {
 	menu_btn.classList.toggle('is-active')
+	mobile_menu.classList.toggle('is-active')
+})
+
+
+
+navButtonCheck.addEventListener('click', function() {
+	menu_btn.classList.remove('is-active')
 	mobile_menu.classList.toggle('is-active')
 })
 
